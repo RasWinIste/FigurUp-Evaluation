93c80bbd1d754be23a30dc716ff31c9bd20eb1f2
Hugo Raskin <raskin.hugo@gmail.com>
2025-08-01 15:23:20 +0200

    Refactor profile

--- FigurUp/profile.py
+++ FigurUp/profile.py
@@ -3,17 +3,37 @@ from datetime import timedelta
 
 from flask import Blueprint, render_template, g, flash, request, session, redirect, url_for
 from sqlalchemy.exc import SQLAlchemyError
-from modelsDB.figuration import Figuration, Compose, Location
 
+from modelsDB.figuration import Figuration, Compose, Location
 from FigurUp.auth import login_required
 from init_db import db
 
 bp = Blueprint('profile', __name__, url_prefix='/profil')
 
+
+def get_name(figuration): 
+    if figuration.fictif:
+        return figuration.fictif.nom
+    if figuration.celebrite:
+        return f"{figuration.celebrite.prenom} {figuration.celebrite.nom}"
+    if figuration.animal:
+        return figuration.animal.nom
+    return None
+
+
+def determine_form_type(client):
+    if client.prive:
+        return 'particulier'
+    if client.professionnel:
+        return 'professionnel'
+    return 'particulier'
+
+
 @bp.route('/')
 @login_required
 def index():
     return render_template('profile/profile.html', client=g.client)
+
 
 @bp.route('/history', methods=['GET', 'POST'])
 @login_required
@@ -28,6 +48,7 @@ def history():
         .order_by(Location.date_heure.desc())
         .all()
     )
+
     grouped = defaultdict(lambda: {
         'date_heure': None,
         'date_fin': None,
@@ -37,27 +58,26 @@ def history():
         'caution': 0.0,
         'names': [],
     })
+
     for location, tarif_heure, caution, figuration in rows:
         location_id = location.id
         duree = float(location.duree)
-        prix = float(tarif_heure) * duree
+        prix = tarif_heure * duree
 
-        if grouped[location_id]['date_heure'] is None:
-            grouped[location_id]['date_heure'] = location.date_heure
-            grouped[location_id]['date_fin'] = location.date_heure + timedelta(hours=duree)
+        location_data = grouped[location_id]
 
-        grouped[location_id]['tarif_heure'] += tarif_heure
-        grouped[location_id]['duree'] = duree
-        grouped[location_id]['prix'] += prix
-        grouped[location_id]['caution'] += float(caution)
-        nom = (
-            figuration.fictif.nom if figuration.fictif else
-            (figuration.celebrite.prenom + " " + figuration.celebrite.nom) if figuration.celebrite else
-            figuration.animal.nom if figuration.animal else
-            None
-        )
+        if location_data['date_heure'] is None:
+            location_data['date_heure'] = location.date_heure
+            location_data['date_fin'] = location.date_heure + timedelta(hours=duree)
+
+        location_data['tarif_heure'] += tarif_heure
+        location_data['duree'] = duree
+        location_data['prix'] += prix
+        location_data['caution'] += float(caution)
+
+        nom = get_name(figuration)
         if nom:
-            grouped[location_id]['names'].append(nom)
+            location_data['names'].append(nom)
 
     resultats = []
     for data in grouped.values():
@@ -66,37 +86,33 @@ def history():
 
     return render_template('profile/history.html', client=g.client, figurations=resultats)
 
+
 @bp.route('/update', methods=['GET', 'POST'])
 @login_required
 def update():
     client = g.client
-    if client.prive:
-        form_type = 'particulier'
-    elif client.professionnel:
-        form_type = 'professionnel'
-    else:
-        form_type = 'particulier'
+    form_type = determine_form_type(client)
 
     if request.method == 'POST':
         data = request.form.to_dict()
 
-        client.mail = data['mail']
-        client.adresse.street = data['street']
-        client.adresse.number = data['number']
-        client.adresse.city = data['city']
-        client.adresse.postalCode = data['postalCode']
-        client.adresse.country = data['country']
+        client.mail = data.get('mail')
+        client.adresse.street = data.get('street')
+        client.adresse.number = data.get('number')
+        client.adresse.city = data.get('city')
+        client.adresse.postalCode = data.get('postalCode')
+        client.adresse.country = data.get('country')
 
-        if form_type == 'particulier':
-            client.prive.firstname = data['firstname']
-            client.prive.lastname = data['lastname']
-            client.prive.phone = data['phone']
-        elif form_type == 'professionnel':
-            client.professionnel.firstname = data['firstname']
-            client.professionnel.lastname = data['lastname']
-            client.professionnel.phone = data['phone']
-            client.professionnel.name = data['name']
-            client.professionnel.tva = data['tva']
+        if form_type == 'particulier' and client.prive:
+            client.prive.firstname = data.get('firstname')
+            client.prive.lastname = data.get('lastname')
+            client.prive.phone = data.get('phone')
+        elif form_type == 'professionnel' and client.professionnel:
+            client.professionnel.firstname = data.get('firstname')
+            client.professionnel.lastname = data.get('lastname')
+            client.professionnel.phone = data.get('phone')
+            client.professionnel.name = data.get('name')
+            client.professionnel.tva = data.get('tva')
 
         try:
             db.session.commit()
@@ -107,12 +123,12 @@ def update():
 
     return render_template('profile/update.html', client=client, form_type=form_type)
 
+
 @bp.route('/unsubscribe', methods=['POST'])
 @login_required
 def unsubscribe():
-    client = g.client
     try:
-        db.session.delete(client)
+        db.session.delete(g.client)
         db.session.commit()
         session.clear()
         return redirect(url_for('auth.signin', deleted='true'))
@@ -120,4 +136,3 @@ def unsubscribe():
         db.session.rollback()
         flash("Une erreur est survenue lors de la suppression du compte.", "error")
         return redirect(url_for('profile.index'))
-
